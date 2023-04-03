988fed0c788f0c4169fc8c0393e197eabe939d75
vimal prakash <vimalprakash332@gamil.com>
2023-04-03 17:33:27 +0530

    add highscore

--- Assets/Script/Logic.cs
+++ Assets/Script/Logic.cs
@@ -8,7 +8,13 @@ public class Logic : MonoBehaviour
     // Start is called before the first frame update
     public int playerscore=0;
     public Text Scoreui;
+    public Text HighScore;
     public GameObject gameovrscreen;
+    public int highscore;
+    private void Start()
+    {
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+    }
     [ContextMenu("add score")]
     public void addscore()
     {
@@ -22,6 +28,12 @@ public class Logic : MonoBehaviour
     }
     public void gameover()
     {
+        if (highscore < playerscore)
+        {
+            PlayerPrefs.SetInt("highscore", playerscore);
+        }
+        HighScore.text=PlayerPrefs.GetInt("highscore", 0).ToString();
         gameovrscreen.SetActive(true);
+       
     }
 }
